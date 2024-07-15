48bbf2e9de787adc57e4f5f35110f4acc7f0a7b4
invictus <invictusnitude@gmail.com>
2024-07-15 15:09:52 +0300

    function short hand

--- elixir/secrets/lib/secrets.ex
+++ elixir/secrets/lib/secrets.ex
@@ -1,29 +1,15 @@
 defmodule Secrets do
-  require Bitwise
+  def secret_add(secret), do: &(&1 + secret)
 
-  def secret_add(secret) do
-    fn param -> secret + param end
-  end
+  def secret_subtract(secret), do: &(&1 - secret)
 
-  def secret_subtract(secret) do
-    fn param -> param - secret end
-  end
+  def secret_multiply(secret), do: &(&1 * secret)
 
-  def secret_multiply(secret) do
-    fn param -> param * secret end
-  end
+  def secret_divide(secret), do: &div(&1, secret)
 
-  def secret_divide(secret) do
-    fn param -> floor(param / secret) end
-  end
+  def secret_and(secret), do: &Bitwise.band(secret, &1)
 
-  def secret_and(secret) do
-  fn param -> Bitwise.band(secret, param) end
-  end
-
-  def secret_xor(secret) do
-     fn param -> Bitwise.bxor(secret, param) end
-  end
+  def secret_xor(secret),  do: &Bitwise.bxor(&1, secret)
 
   def secret_combine(secret_function1, secret_function2) do
     fn param ->
