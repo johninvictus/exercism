1f13b6fa208160bf60357e25cce2a2909be71ab4
invictus <invictusnitude@gmail.com>
2024-10-08 20:15:12 +0300

    improved guess game

--- elixir/guessing-game/lib/guessing_game.ex
+++ elixir/guessing-game/lib/guessing_game.ex
@@ -1,28 +1,27 @@
 defmodule GuessingGame do
-
   def compare(secret_number, guess \\ :guess)
 
-  def compare(_secret_number, guess) when  not is_number(guess) do
+  def compare(_secret_number, guess) when not is_number(guess) do
     "Make a guess"
   end
 
   def compare(secret_number, guess) do
-    do_compare(secret_number, guess, abs(secret_number - guess))
+    do_compare(secret_number, guess)
   end
 
-  defp do_compare(num, num, _difference), do: "Correct"
+  defp do_compare(num, num), do: "Correct"
 
-  defp do_compare(_secret_number, _guess, 1) do
+  defp do_compare(secret_number, guess) when abs(secret_number - guess) == 1 do
     "So close"
   end
 
-  defp do_compare(secret_number, guess, difference)
-       when difference > 1 and guess > secret_number do
+  defp do_compare(secret_number, guess)
+       when guess > secret_number do
     "Too high"
   end
 
-  defp do_compare(secret_number, guess, difference)
-       when difference > 1 and secret_number > guess do
-        "Too low"
+  defp do_compare(secret_number, guess)
+       when secret_number > guess do
+    "Too low"
   end
 end
