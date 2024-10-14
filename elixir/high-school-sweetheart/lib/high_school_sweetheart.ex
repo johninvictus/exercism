28e7fa2058afb2e5d9ef830d657d1a4d42882b91
invictus <invictusnitude@gmail.com>
2024-10-15 00:18:05 +0300

    improve

--- elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
+++ elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -6,18 +6,17 @@ defmodule HighSchoolSweetheart do
   end
 
   def initial(name) do
-    name =
-      name
-      |> first_letter()
-      |> String.upcase()
-
-    name <> "."
+    name
+    |> first_letter()
+    |> String.upcase()
+    |> Kernel.<>(".")
   end
 
   def initials(full_name) do
-    [first, second] = String.split(full_name, " ")
-
-    "#{initial(first)} #{initial(second)}"
+    full_name
+    |> String.split(" ")
+    |> Enum.map(&initial/1)
+    |> Enum.join(" ")
   end
 
   def pair(full_name1, full_name2) do
@@ -25,20 +24,20 @@ defmodule HighSchoolSweetheart do
     n2 = initials(full_name2)
 
     """
-                    ******       ******
-                  **      **   **      **
-                **         ** **         **
-               **            *            **
-               **                         **
-               **     #{n1}  +  #{n2}     **
-                **                       **
-                  **                   **
-                    **               **
-                      **           **
-                        **       **
-                          **   **
-                            ***
-                             *
-               """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{n1}  +  #{n2}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
   end
 end
