32b38421bffc9f4d332a020773ab02fba132bb24
invictus <invictusnitude@gmail.com>
2024-11-06 13:56:53 +0300

    character sheet

---
+++ elixir/rpg-character-sheet/lib/rpg/character_sheet.ex
@@ -0,0 +1,38 @@
+defmodule RPG.CharacterSheet do
+  def welcome() do
+    IO.puts("Welcome! Let's fill out your character sheet together.")
+  end
+
+  def ask_name() do
+    get_console("What is your character's name?\n")
+  end
+
+  def ask_class() do
+    get_console("What is your character's class?\n")
+  end
+
+  def ask_level() do
+    "What is your character's level?\n"
+    |> get_console()
+    |> String.to_integer()
+  end
+
+  def run() do
+    welcome()
+
+    name = ask_name()
+    class = ask_class()
+    level = ask_level()
+
+    body = %{class: class, level: level, name: name}
+
+    IO.puts("Your character: #{inspect(body)}")
+    IO.inspect(body)
+  end
+
+  defp get_console(question) do
+    question
+    |> IO.gets()
+    |> String.trim()
+  end
+end
