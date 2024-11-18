cd9ad0b0824edffdb8c0154ce921d0c71e212090
invictus <invictusnitude@gmail.com>
2024-11-18 22:09:43 +0300

    add logic for pepsi bar

--- elixir/pepsi_bar/lib/pepsi_bar.ex
+++ elixir/pepsi_bar/lib/pepsi_bar.ex
@@ -3,16 +3,34 @@ defmodule PepsiBar do
   Documentation for `PepsiBar`.
   """
 
-  @doc """
-  Hello world.
+  def render(template, options) do
+    do_render(template, "", options)
+  end
 
-  ## Examples
+  defp do_render("", acc, _options), do: acc
 
-      iex> PepsiBar.hello()
-      :world
+  defp do_render(<<"{{", rest::binary>>, acc, options) do
+    {value, rest} = extract_value(rest, "", options)
 
-  """
-  def hello do
-    :world
+    do_render(rest, acc <> value, options)
+  end
+
+  defp do_render(<<char::utf8, rest::binary>>, acc, options) do
+    do_render(rest, acc <> <<char::utf8>>, options)
+  end
+
+  defp extract_value(<<"}}", rest::binary>>, accum, options) do
+    keys =
+      accum
+      |> String.split(".")
+      |> Enum.map(&String.to_atom/1)
+
+      value = get_in(options, keys) || ""
+
+    {value, rest}
+  end
+
+  defp extract_value(<<char::utf8, rest::binary>>, accum, options) do
+    extract_value(rest, accum <> <<char::utf8>>, options)
   end
 end
--- elixir/pepsi_bar/test/pepsi_bar_test.exs
+++ elixir/pepsi_bar/test/pepsi_bar_test.exs
@@ -1,8 +1,45 @@
 defmodule PepsiBarTest do
   use ExUnit.Case
-  doctest PepsiBar
 
-  test "greets the world" do
-    assert PepsiBar.hello() == :world
+  test "nothing" do
+    assert PepsiBar.render("", %{}) == ""
+  end
+
+  test "empty params" do
+    assert PepsiBar.render("some text", %{}) == "some text"
+  end
+
+  test "blank for missing key" do
+    assert PepsiBar.render("{{no_key}} and some text", %{}) == " and some text"
+  end
+
+  test "with key" do
+    assert PepsiBar.render("some {{key}}", %{key: "foo"}) == "some foo"
+  end
+
+  test "duplicate keys" do
+    assert PepsiBar.render("{{key}} and {{key}}!", %{key: "foo"}) == "foo and foo!"
+  end
+
+  test "with full template text" do
+    assert PepsiBar.render("field1: {{foo}}, {{bar}}, {{baz}}", %{
+             foo: "{{foo}}",
+             bar: "this is bar"
+           }) == "field1: {{foo}}, this is bar, "
+  end
+
+  test "with dot notation" do
+    assert PepsiBar.render("abc: {{a.b.c}}, abe: {{a.b.e}}, cde: {{c.d.e}}", %{
+             a: %{b: %{c: "dot"}}
+           }) ==
+             "abc: dot, abe: , cde: "
+  end
+
+  
+  test "with dot notation and two values" do
+    assert PepsiBar.render("abc: {{a.b.c}}, abe: {{a.b.e}}, cde: {{a.c.d.e}}", %{
+             a: %{b: %{c: "dot"}, c: %{d: %{e: "e"}}}
+           }) ==
+             "abc: dot, abe: , cde: e"
   end
 end
